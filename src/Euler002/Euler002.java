c994c8a58e7b04f4da3c18fa2f2e98e61faca102
Alex Hardewig <ahardewig@gmail.com>
2018-06-13 19:56:23 -0400

    Solved Project Euler #002

---
+++ src/Euler002/Euler002.java
@@ -0,0 +1,31 @@
+package Euler002;
+
+/**
+ * @author Alex Hardewig
+ * @date 06/13/2018
+ */
+public class Euler002 {
+
+    public long recursiveFibonnaci(){
+        long prevprev;
+        long previous = 0;
+        long current = 1;
+        long total = 0;
+
+        while(true){
+            if (current >= 4000000){
+                return total;
+            }
+
+            prevprev = previous;
+            previous = current;
+            current = previous + prevprev;
+
+            if (current % 2 == 0) {
+                total += current;
+            }
+        }
+    }
+
+
+}
