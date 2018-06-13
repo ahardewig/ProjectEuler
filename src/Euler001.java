ad3181d96ec1f7d5ff9b3415e57f3ce5b3986478
Alex Hardewig <ahardewig@gmail.com>
2018-06-13 19:34:54 -0400

    Finished Euler Project #001

--- src/Euler001.java
+++ src/Euler001.java
@@ -1,10 +1,27 @@
 /**
- * Program info
  *
- * @author Alex Hardewig, ahardewi@purdue.edu, lab sec G03
- * @version date
+ * @author Alex Hardewig
+ * @date 6/13/18
  */
 public class Euler001 {
 
+    public int findSumOfMultiples() {
+        int total = 0;
+        for (int i = 1; i < 1000; i++){
+            if (isMultipleOf3(i) || isMultipleOf5(i)){
+                total += i;
+            }
+        }
+        return total;
+    }
+
+
+    private boolean isMultipleOf3(int number){
+        return (number % 3 == 0);
+    }
+
+    private boolean isMultipleOf5(int number){
+        return (number % 5 == 0);
+    }
 
 }
